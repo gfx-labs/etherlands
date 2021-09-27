4079803ca951a0c9b3f06aee758ccbaf087ec26e
elee <eddy@gfxlabs.io>
2021-09-26 19:10:28 -0500

    no way right

--- worldzmq.go
+++ worldzmq.go
@@ -22,7 +22,7 @@ type VarArgs []string
 
 func StartWorldZmq(world *types.World) error {
 	publisher := goczmq.NewPubChanneler("tcp://*:10105")
-	subscriber := goczmq.NewSubChanneler("tcp://127.0.0.1:10106", "")
+	subscriber := goczmq.NewSubChanneler("tcp://127.0.0.1:10106", "GET")
 	zmq := &WorldZmq{W: world, publisher: publisher, subscriber: subscriber}
 	go zmq.StartPublishing()
 	go zmq.StartListening()
