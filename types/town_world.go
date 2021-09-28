8c05af5e101fee71c1b1552dd966b7baed17d877
elee <eddy@gfxlabs.io>
2021-09-28 06:35:06 -0500

    no clue

--- types/town_world.go
+++ types/town_world.go
@@ -106,7 +106,7 @@ func (W *World) LoadTown(name string) (*Town, error) {
 		read_town.Teams(&team, i)
 		team_members := make(map[uuid.UUID]struct{})
 		for j := 0; j < team.MembersLength(); j++ {
-			puuid := proto.UUID{}
+			var puuid proto.UUID
 			new_uuid := ProtoResolveUUID(&puuid)
 			team_members[new_uuid] = struct{}{}
 		}
